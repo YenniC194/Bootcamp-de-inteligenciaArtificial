d67faac40ef4991112f2fe964d08e27054abcb60
YenniC194 <yennipat22@gmail.com>
2024-11-07 17:11:19 -0500

    abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,11 @@
+"string" #string se utiliza para cadenas de texto
+"hola mundo"
+"""Tus datos son:
+nombre: Yenni 
+Apellido: Cumbalaza"""
+#En python tenemos diferentes tipos de numeros 
+4 #numero entero
+40.2 #numero tipo float
+#tambien tenemos el tipo de datos boolean
+True #verdadero 
+False #falso
